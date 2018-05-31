313807c12cb0dec08e6d254b4c209bf1ab7bf208
Shaidoku <37822545+Shaidoku@users.noreply.github.com>
2018-05-31 00:42:41 -0500

    1

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -11,15 +11,12 @@ import { MainPage } from '../';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account: { email: string, password: string } = {
+
+    account: { email: string, password: string } = {
     email: 'test@example.com',
     password: 'test'
-  };
+    };
 
-  // Our translated text strings
   private loginErrorString: string;
 
   constructor(public navCtrl: NavController,
@@ -30,10 +27,9 @@ export class LoginPage {
     this.translateService.get('LOGIN_ERROR').subscribe((value) => {
       this.loginErrorString = value;
     })
-  }
+}
 
-  // Attempt to login in through our User service
-  doLogin() {
+doLogin() {
     this.user.login(this.account).subscribe((resp) => {
       this.navCtrl.push(MainPage);
     }, (err) => {
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
 
-/**
- * The Welcome Page is a splash page that quickly describes the app,
- * and then directs the user to create an account or log in.
- * If you'd like to immediately put the user onto a login/signup page,
- * we recommend not using the Welcome page.
-*/
+import { User } from '../../providers';
+import { MainPage } from '../';
+
 @IonicPage()
 @Component({
   selector: 'page-welcome',
@@ -14,7 +12,37 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController) { }
+	account: { email: string, password: string } = {
+    email: 'ing.antonio.gutierrezgarcia@outlook.com',
+    password: 'ha756STRfa5'
+    };
+
+    private loginErrorString: string;
+
+  constructor(public navCtrl: NavController,
+    public user: User,
+    public toastCtrl: ToastController,
+    public translateService: TranslateService) {
+
+    this.translateService.get('LOGIN_ERROR').subscribe((value) => {
+      this.loginErrorString = value;
+    }) 
+}
+
+doLogin() {
+    this.user.login(this.account).subscribe((resp) => {
+      this.navCtrl.push(MainPage);
+    }, (err) => {
+      this.navCtrl.push(MainPage);
+      // Unable to log in
+      let toast = this.toastCtrl.create({
+        message: this.loginErrorString,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    });
+  }
 
   login() {
     this.navCtrl.push('LoginPage');
