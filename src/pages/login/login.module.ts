027444940d65282ca751936cac0189ab568c5874
Antonio <ing.antonio.gutierrezgarcia@outlook.com>
2018-05-31 21:43:52 -0500

    Added Facebook SingUp Code

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -4,6 +4,53 @@ import { IonicPageModule } from 'ionic-angular';
 
 import { LoginPage } from './login';
 
+// callbackManager = CallbackManager.Factory.create();
+//
+//
+//     loginButton = (LoginButton) findViewById(R.id.login_button);
+//     loginButton.setReadPermissions("email");
+//     // If using in a fragment
+//     loginButton.setFragment(this);
+//
+//     // Callback registration
+//     loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//         @Override
+//         public void onSuccess(LoginResult loginResult) {
+//             // App code
+//         }
+//
+//         @Override
+//         public void onCancel() {
+//             // App code
+//         }
+//
+//         @Override
+//         public void onError(FacebookException exception) {
+//             // App code
+//         }
+//     });
+//
+//
+//     callbackManager = CallbackManager.Factory.create();
+//
+//     LoginManager.getInstance().registerCallback(callbackManager,
+//             new FacebookCallback<LoginResult>() {
+//                 @Override
+//                 public void onSuccess(LoginResult loginResult) {
+//                     // App code
+//                 }
+//
+//                 @Override
+//                 public void onCancel() {
+//                      // App code
+//                 }
+//
+//                 @Override
+//                 public void onError(FacebookException exception) {
+//                      // App code
+//                 }
+//     });
+
 @NgModule({
   declarations: [
     LoginPage,
