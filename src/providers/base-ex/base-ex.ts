fc0a37bf82be0b2f1a125254ec8222d11c841438
Joan Ortiz <37822545+Shaidoku@users.noreply.github.com>
2018-10-29 19:53:35 -0600

    07:53 pm
    29/10/2018

--- src/providers/base-ex/base-ex.ts
+++ src/providers/base-ex/base-ex.ts
@@ -7,17 +7,17 @@ export class BaseExProvider {
   constructor(public http: HttpClient)
    {console.log('Hello BaseExProvider Provider');}
 
-calDatos(){return this.http.get('http://162.255.85.22/plesk-site-preview/emprendedoreslp.com/162.255.85.22/api/events');}
+calDatos(){return this.http.get('http://192.168.100.7:8080/api/events');}
 
-usuDatos(){return this.http.get('http://162.255.85.22/plesk-site-preview/emprendedoreslp.com/162.255.85.22/api/users');}
+usuDatos(){return this.http.get('http://192.168.100.7:8080/api/users');}
 
-proDatos(){return this.http.get('http://162.255.85.22/plesk-site-preview/emprendedoreslp.com/162.255.85.22/api/projects');}
+proDatos(){return this.http.get('http://192.168.100.7:8080/api/projects');}
 
-notDatos(){return this.http.get('http://162.255.85.22/plesk-site-preview/emprendedoreslp.com/162.255.85.22/api/noticias');}
+notDatos(){return this.http.get('http://192.168.100.7:8080/api/noticias');}
 //http://emprendedoresslp.test/api/not http://192.168.100.7:8080/api/not http://192.168.100.6:8080/api/not
 
-menDatos(){return this.http.get('http://162.255.85.22/plesk-site-preview/emprendedoreslp.com/162.255.85.22/api/messages');}
+menDatos(){return this.http.get('http://192.168.100.7:8080/api/messages');}
 
-chaDatos(){return this.http.get('http://162.255.85.22/plesk-site-preview/emprendedoreslp.com/162.255.85.22/api/chats');}
+chaDatos(){return this.http.get('http://192.168.100.7:8080/api/chats');}
 }
 // JoanOrtizITSLP@outlook.com
